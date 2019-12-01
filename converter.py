72dff1980209af83d2980d0eb4d12c5004d971d3
Seika139 <tcoanzu@gmail.com>
2019-12-02 00:34:39 +0900

    maybe ok

--- converter.py
+++ converter.py
@@ -5,18 +5,19 @@ import codecs
 import json
 import os
 import pandas as pd
+import re
 
 import ui_operator
 
-SAVE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)),'json-files')
-os.makedirs(SAVE_DIR,exist_ok=True)
-
 excel = ui_operator.select_xlsx()
-input_file = pd.ExcelFile(excel)
+root = os.path.abspath(os.path.dirname(__file__))
+filename = re.split(f'{os.sep}|\.',excel)[-2]
+save_dir = os.path.join(root,'json-files',filename)
+os.makedirs(save_dir,exist_ok=True)
 
-for sheet_name in input_file.sheet_names:
+for sheet_name in pd.ExcelFile(excel).sheet_names:
     df = pd.read_excel(excel, sheet_name=sheet_name,index_col=0)
     dic = df.to_dict()
-    with codecs.open(os.path.join(SAVE_DIR, f'{sheet_name}.json'),'w',encoding='utf-8') as f:
+    with codecs.open(os.path.join(save_dir, f'{sheet_name}.json'),'w',encoding='utf-8') as f:
         dump = json.dumps(dic,indent=2,ensure_ascii=False)
         f.write(dump)
