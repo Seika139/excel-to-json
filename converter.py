fe2abeded34ca54fd0d78d803bf64195d47c19fc
Seika139 <tcoanzu@gmail.com>
2019-12-02 00:40:19 +0900

    maybe ok

--- converter.py
+++ converter.py
@@ -9,14 +9,14 @@ import re
 
 import ui_operator
 
-def main(excel_path):
-    root = os.path.abspath(os.path.dirname(__file__))
-    filename = re.split(f'{os.sep}|\.',excel_path)[-2]
-    save_dir = os.path.join(root,'json-files',filename)
+def main(path_xlsx):
+    root_dir = os.path.abspath(os.path.dirname(__file__))
+    child_dir = re.split(f'{os.sep}|\.',path_xlsx)[-2]
+    save_dir = os.path.join(root_dir,'json-files',child_dir)
     os.makedirs(save_dir,exist_ok=True)
 
-    for sheet_name in pd.ExcelFile(excel_path).sheet_names:
-        df = pd.read_excel(excel_path, sheet_name=sheet_name,index_col=0)
+    for sheet_name in pd.ExcelFile(path_xlsx).sheet_names:
+        df = pd.read_excel(path_xlsx, sheet_name=sheet_name,index_col=0)
         dic = df.to_dict()
         with codecs.open(os.path.join(save_dir, f'{sheet_name}.json'),'w',encoding='utf-8') as f:
             dump = json.dumps(dic,indent=2,ensure_ascii=False)
